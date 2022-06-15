65f595579c956843f477815f211d1102bf6a459e
Anthony <lynkfox@gmail.com>
2022-06-15 16:54:19 -0400

    Some more refactoring for class and with_allies tests are green

--- all_tests/unit_tests/test_rds_query_helpers.py
+++ all_tests/unit_tests/test_rds_query_helpers.py
@@ -24,19 +24,55 @@ class Test_character_is:
         test_response = character_is("Absolute Zero", Type.HERO, SqlTables.HERO_TEAMS)
         assert (
             test_response
-            == '"Absolute Zero" IN (heroTeams.hero_one, heroTeams.hero_two, heroTeams.hero_three, heroTeams.hero_four, heroTeams.hero_five)'
+            == "`Absolute Zero` IN (heroTeams.hero_one, heroTeams.hero_two, heroTeams.hero_three, heroTeams.hero_four, heroTeams.hero_five)"
         )
 
     def test_with_villain_and_no_table_returns_correct_string(self):
         test_response = character_is("Baron Blade", Type.VILLAIN)
         assert (
             test_response
-            == '"Baron Blade" IN (gameDetails.villain_one, gameDetails.villain_two, gameDetails.villain_three, gameDetails.villain_four, gameDetails.villain_five)'
+            == "`Baron Blade` IN (gameDetails.villain_one, gameDetails.villain_two, gameDetails.villain_three, gameDetails.villain_four, gameDetails.villain_five)"
         )
 
     def test_with_villain_and_opponent_table_returns_correct_string(self):
         test_response = character_is("Baron Blade", Type.VILLAIN, SqlTables.OPPONENTS)
         assert (
             test_response
-            == '"Baron Blade" IN (opponents.villain_one, opponents.villain_two, opponents.villain_three, opponents.villain_four, opponents.villain_five)'
+            == "`Baron Blade` IN (opponents.villain_one, opponents.villain_two, opponents.villain_three, opponents.villain_four, opponents.villain_five)"
+        )
+
+
+class Test_with_allies:
+    def test_returns_str(self):
+        assert isinstance(with_allies(["Absolute Zero"], Type.HERO), str)
+
+    def test_returns_valid_string_with_only_one_hero(self):
+        test_response = with_allies(["Absolute Zero"], Type.HERO)
+        assert test_response == "heroTeams.hero_one=`Absolute Zero`"
+
+    def test_returns_valid_string_with_more_than_one_hero(self):
+        test_response = with_allies(["Absolute Zero", "Bunker"], Type.HERO)
+        assert (
+            test_response
+            == "heroTeams.hero_one=`Absolute Zero`, heroTeams.hero_two=`Bunker`"
+        )
+
+    def test_same_order_if_positional_true(self):
+        test_response = with_allies(["Writhe", "Bunker"], Type.HERO, positional=True)
+        assert (
+            test_response
+            == "gameDetails.hero_one=`Writhe`, gameDetails.hero_two=`Bunker`"
+        )
+
+    def test_sorts_if_positional_left_to_default(self):
+        test_response = with_allies(["Writhe", "Bunker"], Type.HERO)
+        assert (
+            test_response == "heroTeams.hero_one=`Bunker`, heroTeams.hero_two=`Writhe`"
+        )
+
+    def test_villains_points_to_opponent_table(self):
+        test_response = with_allies(["Baron Blade", "Ermine"], Type.VILLAIN)
+        assert (
+            test_response
+            == "opponents.villain_one=`Baron Blade`, opponents.villain_two=`Ermine`"
         )
--- common/rds/queries_gen.py
+++ common/rds/queries_gen.py
@@ -1,6 +1,7 @@
 from common.models.enums import Type
 from common.sql_attributes import SqlTables
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass(frozen=True)
@@ -10,21 +11,58 @@ class ColumnGroup:
     """
 
     @classmethod
-    def team_members(
-        cls, prefix: Type = Type.HERO, table_name: SqlTables = SqlTables.GAME_DETAILS
+    def team_member(
+        cls,
+        prefix: Type = Type.HERO,
+        table_name: SqlTables = SqlTables.GAME_DETAILS,
     ):
         """
         shortcut for the same column names used over and over again.
         """
+
+        return ", ".join(cls.team_columns(prefix, table_name))
+
+    @classmethod
+    def team_columns(
+        cls, prefix: Type = Type.HERO, table_name: SqlTables = SqlTables.GAME_DETAILS
+    ) -> list:
+        """
+        returns a list of columns in order all prefixed, for use in only needing a few of them for positional lookups
+        """
+        cls._is_hero_or_villain(prefix)
+        cls._is_hero_or_villain_team_table(table_name)
+
+        suffixes = ["_one", "_two", "_three", "_four", "_five"]
+
         column_prefix = f"{table_name}.{prefix.value.lower()}"
-        return f"{column_prefix}_one, {column_prefix}_two, {column_prefix}_three, {column_prefix}_four, {column_prefix}_five"
+
+        return [column_prefix + suffix for suffix in suffixes]
+
+    @classmethod
+    def _is_hero_or_villain(cls, prefix: Type):
+        """
+        Error Handling for hero_[number] column names.
+        """
+        if prefix is not Type.HERO and prefix is not Type.VILLAIN:
+            raise ValueError("type Mismatch: must be HERO or VILLAIN")
+
+    @classmethod
+    def _is_hero_or_villain_team_table(cls, table_name: SqlTables):
+        if (
+            table_name is not SqlTables.HERO_TEAMS
+            and table_name is not SqlTables.GAME_DETAILS
+            and table_name is not SqlTables.OPPONENTS
+        ):
+            raise ValueError(
+                "Table Mismatch: table_name must be GAME_DETAILS, HERO_TEAMS, or OPPONENTS"
+            )
 
 
 def character_is(
     character_name: str,
     prefix: Type = Type.HERO,
     table_name: SqlTables = SqlTables.GAME_DETAILS,
-):
+) -> str:
     """
     For Where clause of any sql query to find a character name in all the team names
 
@@ -34,17 +72,32 @@ def character_is(
         table_name (common.sql_attributes.SqlTables): the table to pull these columns from. SqlTables.GAME_DETAILS, SqlTables.HERO_TEAMS, SqlTables.OPPONENTS
 
     """
+    return f"`{character_name}` IN ({ColumnGroup.team_member(prefix, table_name)})"
 
-    if prefix is not Type.HERO and prefix is not Type.VILLAIN:
-        raise ValueError("type Mismatch: must be HERO or VILLAIN")
 
-    if (
-        table_name is not SqlTables.HERO_TEAMS
-        and table_name is not SqlTables.GAME_DETAILS
-        and table_name is not SqlTables.OPPONENTS
-    ):
-        raise ValueError(
-            "Table Mismatch: table_name must be GAME_DETAILS, HERO_TEAMS, or OPPONENTS"
+def with_allies(names: List[str], prefix: Type = Type.HERO, positional=False) -> str:
+    """
+    using character_is and a list of up to 5 names builds a team query string
+
+    Parameters:
+        names (List[str]): common.rds.character_full_name() for each  character name. If positional is True, then the order will be respected.
+        prefix (common.enums.Type): the type, Type.HERO or Type.VILLAIN
+        positional (bool): (default: false) if True, maintains the same order of names and atomically sets the table as GAME_DETAILS,
+            otherwise alphabetizes the list and sets table to HERO_TEAMS or OPPONENTS depending on Type
+    """
+
+    if not positional:
+        names.sort()
+        table_name = (
+            SqlTables.HERO_TEAMS if prefix is Type.HERO else SqlTables.OPPONENTS
         )
 
-    return f'"{character_name}" IN ({ColumnGroup.team_members(prefix, table_name)})'
+    else:
+        table_name = SqlTables.GAME_DETAILS
+
+    columns = ColumnGroup.team_columns(prefix, table_name)[: len(names)]
+
+    if len(names) == 1:
+        return f"{columns[0]}=`{names[0]}`"
+
+    return ", ".join([f"{value}=`{names[i]}`" for i, value in enumerate(columns)])
